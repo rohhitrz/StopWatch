f2697e9f24c413f6831cd1a62fdfe38315a0ba39
Rohit <rohhit.rz@gmail.com>
2024-08-18 22:21:17 +0530

    fixed time format

--- stopwatch/src/stopwatch.jsx
+++ stopwatch/src/stopwatch.jsx
@@ -7,7 +7,7 @@ function Stopwatch(){
     const formatTime=(seconds)=>{
         const minutes= Math.floor(seconds/60);
         const secs=seconds%60;
-        return `${minutes}: ${secs<10 ? '0':'' }${secs}`;
+        return `${minutes}:${secs<10?'0':''}${secs}`;
 
     }
 
